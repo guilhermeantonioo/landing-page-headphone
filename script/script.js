8ae6ad8d96034f8cf1cd7cc3576092cc805af843
Guilherme Antônio <127310615+guilhermeantonioo@users.noreply.github.com>
2024-02-27 01:16:45 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,31 @@
+let btn1 = document.querySelector('.img1')
+let btn2 = document.querySelector('.img2')
+let btn3 = document.querySelector('.img3')
+let block = document.querySelector('.block');
+let btnMenu = document.querySelector('#menu')
+let menu = document.querySelector('.navegacao-menu')
+let container = document.querySelector('.container')
+
+// btnMenu.addEventListener('click' , () =>{
+//     menu.style = 'margin-top:-30rem;'
+// })
+
+btnMenu.addEventListener('click' , () =>{
+    menu.classList.toggle('hide')
+    
+})
+
+btn1.addEventListener('click' , () =>{
+    console.log('clicou na img 1')
+    block.src = 'img/product1.png'
+})
+
+btn2.addEventListener('click' , () =>{
+    console.log('clicou na img 2')
+    block.src = 'img/product2.png'
+})
+
+btn3.addEventListener('click' , () =>{
+    console.log('clicou na img 3')
+    block.src = 'img/product3.png'
+})
